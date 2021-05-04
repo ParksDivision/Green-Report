7eb06cc3b3adc407e8f67fe4408ff1ccea1d5559
ParksDivision <KyleJohnParks@gmail.com>
2021-05-03 19:21:40 -0500

    TODO: configure ts-jest to work with Nuxt/ts plugins

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,12 @@ module.exports = {
   moduleNameMapper: {
     '^~/(.*)$': '<rootDir>/./$1',
     '^@/(.*)$': '<rootDir>/./$1',
-  }
+    '^vue$': 'vue/dist/vue.common.js',
+  }, 
+  moduleFileExtensions: ['ts', 'js', 'vue', 'json'],
+  transform: {
+    // might need to add file pre-processor configs here
+  }, 
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$'
 };
  
